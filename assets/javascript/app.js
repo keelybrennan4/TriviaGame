5f7f0d4601f1527af5b429fa2ebda43a1269f473
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-02-25 22:11:02 -0600

    added trivia multidimensional array and loops to itterate over each round

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,78 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 //define global variables for scoring 
+var questionCount = 0;
     //var currentQuestion = 0;
     //var correctAnswers = 0;
     //var incorrectAnswers = 0;
     //var complete = false;
+
+// define trivia question and answers in multidimensional array
+var trivia = [
+    ["What is the answer to question 1?", "apple", "apple", "Blue", "Red", "Purple"],
+    ["What is the answer to question 2?", "bird", "bird", "Cat", "Dog", "Mouse"],
+    ["What is the answer to question 3?", "cat", "cat", "Bed", "Pillow", "Couch"],
+    ["What is the answer to question 4?", "dog", "dog", "Car", "Van", "Taxi"],
+    ["What is the answer to question 5?", "egg", "egg", "Car", "Van", "Taxi"],
+    ["What is the answer to question 6?", "fish", "fish", "Car", "Van", "Taxi"],
+    ["What is the answer to question 7?", "gold", "gold", "Car", "Van", "Taxi"],
+    ["What is the answer to question 8?", "horse", "horse", "Car", "Van", "Taxi"],
+    ["What is the answer to question 9?", "ice", "ice", "Car", "Van", "Taxi"],
+    ["What is the answer to question 10?", "jack", "jack", "Car", "Van", "Taxi"],
+]
+
+// to initialize the game
+function initialize(){
+    questionCount ++;
+    console.log(questionCount);
+}
+
+//itterate through var trivia to determine the question (index 0++ of var trivia)
+for (var i=0; i < trivia.length; i++) {
+    $("#question").html(trivia[i][0]);
+    if (trivia.length[i] === questionCount) {
+        break;
+    }
+    console.log(trivia[i][0]);
+
+}
+//itterate through var trivia to determine the question (index 0++ of var trivia)
+for (var i=0; i < questionCount; i++) {
+    $("#option1").html(trivia[i][3]);
+    $("#option2").html(trivia[i][4]);
+    $("#option3").html(trivia[i][5]);
+    $("#option4").html(trivia[i][6]);
+    console.log(trivia[0][0]);
+}
+
+
+//to compare if selection is correct, we will convert the answer to a string and compare the string to string of index 2, which is always the correct answer
+
+// $("#question").html(trivia[1][1]);
+//$("#option1").html(trivia[2][2]);
+//$("#option2").html(trivia[3][3]);
+//$("#option3").html(trivia[0][3]);
+//$("#option4").html(trivia[0][4]);
+
+//$("#results").html(trivia[0][1]);
+
+
+
+
+// if (trivia[1) 
+
+// console.log(trivia[0][1]);
+
+});
+
+
+
+// -------  html ids ----------
+// "#timer-display"
+// "#pre-game"
+// "#post-game"
+// "#game"
+// ""
 
 //---------- functions -------------- 
 
@@ -28,7 +96,11 @@ $( document ).ready(function() {
 
 //3. function askQuestion: 
     //show countdown timer and begin counting down from 30 seconds 
+    //increment the question count by 1
         //if time = 0 before an option button is clicked, user gets the questin wrong (wrongAnswer)
+           // if(timeRemaining > 0){
+           //}
+
     //four options will be displayed -- when hovering over each option, the option is selected. 
 
 // 4. function evaluateAnswer: 
